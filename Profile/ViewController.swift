aa4e19463224d59f929fcc41e04d061814b9a00f
Souta Kawamura <soutakawamura@icloud.com>
2020-09-21 07:37:30 +0900

    Initial Commit

---
+++ Profile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Profile
+//
+//  Created by チップまん on 2020/09/21.
+//  Copyright © 2020 com.litech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
